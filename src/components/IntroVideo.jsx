cf847c38c8aad61478bc0a20810b1d6dccbbd28d
Christopher Smith <itar1987@gmail.com>
2024-12-31 12:50:08 -0500

    adjust video size

--- src/components/IntroVideo.jsx
+++ src/components/IntroVideo.jsx
@@ -5,7 +5,7 @@ import jeoopardyVideo from "../Jeopardy_Intro_Short2.mp4";
 export default function IntroVideo(props) {
     return (
       <div className="video-modal">
-        <video src={jeoopardyVideo} width="100%" height="100%" autoPlay onEnded={() => props.videoEnded(1)} onClick={() => props.videoEnded(1)} />
+        <video src={jeoopardyVideo} autoPlay onEnded={() => props.videoEnded(1)} onClick={() => props.videoEnded(1)} />
       </div>
     );
 };
